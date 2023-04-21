91833c88512050658d555970883cf647d3a39f2b
Marcin Dabek <dabudab@gmail.com>
2023-04-22 01:56:10 +0200

    git init

---
+++ app.js
@@ -0,0 +1,26 @@
+const correctAnswers = ["B", "B", "B", "B"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	let score = 0;
+	const userAnswers = [
+		form.q1.value,
+		form.q2.value,
+		form.q3.value,
+		form.q4.value,
+	];
+
+	//check answers
+	userAnswers.forEach((answer, index) => {
+		if (answer === correctAnswers[index]) {
+			score += 25;
+		}
+	});
+	// console.log(score);
+	result.querySelector("span").textContent = `${score}%`;
+	result.classList.remove("d-none");
+	scrollTo(0, 0);
+});
